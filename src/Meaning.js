65e7ea8cfd8dfb8b96fa8ed77b0d31187fca4e69
TDenisaM <99595805+TDenisaM@users.noreply.github.com>
2022-04-27 11:54:01 +0200

    synonyms componenet

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,26 +1,29 @@
 import React from "react";
 
+import Synonyms from "./Synonyms.js";
+
 import "./Meaning.css";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
-    <div className=" mt-5 Meaning">
+    <div className="mt-5 Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            <div>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
+              <strong>Exemples: </strong>
               <em>{definition.exemple}</em>
               <br />
-              {definition.synonyms}
-            </p>
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
           </div>
         );
       })}
-      <h3>Synonyms:</h3>
-      <p>{props.meaning.synonyms}</p>
     </div>
   );
 }
