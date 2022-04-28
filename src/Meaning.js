3426528e10090445c9adfe062eb561f9e25f4a20
TDenisaM <99595805+TDenisaM@users.noreply.github.com>
2022-04-28 14:07:22 +0200

    adjusted numer of definition to be displayed

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,18 +11,22 @@ export default function Meaning(props) {
       <div className="mt-3 Meaning">
         <h2>{props.meaning.partOfSpeech}</h2>
         {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <div className="definition">
-                <strong>{definition.definition}</strong>
-                <br />
-                <small>{definition.example}</small>
-                <div>
-                  <Synonyms synonyms={props.meaning.synonyms} />
+          if (index < 5 && index > 0) {
+            return (
+              <div key={index}>
+                <div className="definition">
+                  <strong>{definition.definition}</strong>
+                  <br />
+                  <small>{definition.example}</small>
+                  <div>
+                    <Synonyms synonyms={props.meaning.synonyms} />
+                  </div>
                 </div>
               </div>
-            </div>
-          );
+            );
+          } else {
+            return null;
+          }
         })}
         <Synonyms synonyms={props.meaning.synonyms} />
       </div>
